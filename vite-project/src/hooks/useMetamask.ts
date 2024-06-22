caaafcd3f08621522bbef548e94d8812fec1f170
rafa <rafa.canseco@gmail.com>
2024-06-21 19:22:01 -0600

    frontend types

--- vite-project/src/hooks/useMetamask.ts
+++ vite-project/src/hooks/useMetamask.ts
@@ -1,14 +1,13 @@
 import { useState, useEffect } from "react";
 import AccountService from "../services/AccountService";
-import { JsonRpcSigner } from "@ethersproject/providers";
 
 const useMetamask = () => {
-const [signer, setSigner] = useState<JsonRpcSigner | null>(null);
+  const [signer, setSigner] = useState<any | null>(null);
   const [walletAddress, setWalletAddress] = useState<string | null>(null);
 
   const connectMetamask = async () => {
     const { signer, walletAddress } = await AccountService.connectWallet();
-    setSigner(signer as unknown as JsonRpcSigner);
+    setSigner(signer);
     setWalletAddress(walletAddress);
     return { signer, walletAddress };
   };
@@ -16,7 +15,7 @@ const [signer, setSigner] = useState<JsonRpcSigner | null>(null);
   const tryConnectingMetamask = async () => {
     const { signer, walletAddress } = await AccountService.getAccountData();
     if (signer && walletAddress) {
-        setSigner(signer as unknown as JsonRpcSigner);
+        setSigner(signer);
         setWalletAddress(walletAddress);
       }
     return { signer, walletAddress };
